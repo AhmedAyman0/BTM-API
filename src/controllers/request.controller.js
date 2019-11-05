63c7d7df8dc35a29531c0f1c8ad59d5d353ee20c
Ahmed Ayman <ahmed.a.elkemary@gmail.com>
2019-11-05 13:51:12 +0200

    edit requests

--- src/controllers/request.controller.js
+++ src/controllers/request.controller.js
@@ -1,53 +1,54 @@
+var Request = require("../models/request");
 
-var Request = require('../models/request');
+exports.getAll = async (req, res) => {
+  try {
+    const requests = await Request.find();
+    return res.status(200).json(requests);
+  } catch (error) {
+    return res.status(500).json({ msg: error.message });
+  }
+};
 
-exports.getAll = async (req,res)=>{
-    try {
-        const requests = await Request.find();
-        return res.status(200).json(requests);
-    } catch (error) {
-        return res.status(500).json({msg : error.message})
-    }
+exports.getByIdFor = async (req, res) => {
+  try {
+    const requests = await Request.find({
+      $or: [{ to: req.params.id }, { from: req.params.id }]
+    })
+      .populate("from")
+      .populate("to")
+      .populate("item");
+    return res.status(200).json(requests);
+  } catch (error) {
+    return res.status(500).json({ msg: error.message });
+  }
+};
+exports.createRequest = async (req, res) => {
+  try {
+    const request = await Request.create(req.body);
 
-}
+    return res.status(200).json(request);
+  } catch (error) {
+    return res.status(500).json({ msg: error.message });
+  }
+};
+exports.updateRequest = async (req, res) => {
+  try {
+    const request = await Request.findByIdAndUpdate(
+      req.params.id,
+      req.body
+    ).populate("requests");
 
+    return res.status(200).json(request);
+  } catch (error) {
+    return res.status(500).json({ msg: error.message });
+  }
+};
+exports.deleteRequest = async (req, res) => {
+  try {
+    const request = await Request.findByIdAndDelete(req.params.id);
 
-exports.getByIdFor = async (req,res)=>{
-    try {
-        
-        const requests = await Request.find({$or:[{to:req.params.id},{from:req.params.id}]}).populate('from').populate('item');
-        return res.status(200).json(requests);
-    } catch (error) {
-        return res.status(500).json({msg : error.message})
-    }
-}
-exports.createRequest = async (req,res)=>{
-    try {
-
-        const request = await Request.create(req.body);
-
-        return res.status(200).json(request);
-    } catch (error) {
-        return res.status(500).json({msg : error.message})
-    }
-}
-exports.updateRequest = async (req,res)=>{
-    try {
-        
-        const request = await Request.findByIdAndUpdate(req.params.id,req.body).populate('requests');
-
-        return res.status(200).json(request);
-    } catch (error) {
-        return res.status(500).json({msg : error.message})
-    }
-}
-    exports.deleteRequest = async (req,res)=>{
-        try {
-            
-            const request = await Request.findByIdAndDelete(req.params.id);
-    
-            return res.status(200).json(request);
-        } catch (error) {
-            return res.status(500).json({msg : error.message})
-        }
-}
+    return res.status(200).json(request);
+  } catch (error) {
+    return res.status(500).json({ msg: error.message });
+  }
+};
