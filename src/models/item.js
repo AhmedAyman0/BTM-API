10e3ea55ed5781fd21d25cb5b587726d4afca0ac
Ahmed Ayman <ahmed.a.elkemary@gmail.com>
2019-11-07 14:28:39 +0200

    add price to item

--- src/models/item.js
+++ src/models/item.js
@@ -1,44 +1,43 @@
-var mongoose = require('mongoose');
-var bcrypt = require('bcrypt');
+var mongoose = require("mongoose");
+var bcrypt = require("bcrypt");
 var Schema = mongoose.Schema;
-  
+
 var ItemSchema = new mongoose.Schema({
   name: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true,
-        
-    },
-    imgUrl:{
-        type:String,
-        default:'assets/imgs/item.png'
-    },
-    price:{
-        type:Number,
-        default:10.0
-    }
-,
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  imgUrl: {
+    type: String,
+    default: "assets/imgs/item.png"
+  },
+  price: {
+    type: Number,
+    default: 10.0
+  },
   shop: {
-        type: Schema.Types.ObjectId,
-        ref: 'Shop'
-    },
-    count:{
-        type:Number,
-        required:true,
-        default:1
-    },
-    inStock:{
-        type:Boolean,
-        default:true,
-    },
-    description:{
-        type:String,
-        required:false,
-    }
+    type: Schema.Types.ObjectId,
+    ref: "Shop"
+  },
+  color:{
+      type:String,
+      default:'white'
+  },
+  count: {
+    type: Number,
+    required: true,
+    default: 1
+  },
+  inStock: {
+    type: Boolean,
+    default: true
+  },
+  description: {
+    type: String,
+    required: false
+  }
 });
- 
 
-
- 
-module.exports = mongoose.model('Item', ItemSchema);
+module.exports = mongoose.model("Item", ItemSchema);
