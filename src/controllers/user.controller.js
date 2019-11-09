d36a7991fecfdfc19d6f8d4f6eb52dfb8fa83de8
Ahmed Ayman <ahmed.a.elkemary@gmail.com>
2019-11-09 16:33:07 +0200

    add change password

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,7 @@
 var User = require("../models/user");
 var ResetPassword = require("../models/forgetPassword");
 var jwt = require("jsonwebtoken");
-let bcrypt = require('bcrypt');
+let bcrypt = require("bcrypt");
 
 var crypto = require("crypto");
 const { OAuth2Client } = require("google-auth-library");
@@ -9,18 +9,21 @@ var config = require("../config/config");
 const nodemailer = require("nodemailer");
 let transporter = nodemailer.createTransport({
   service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
-    user: "e7tiaty2001@gmail.com", // generated ethereal user
-    pass: "realismydna123" // generated ethereal password
+    user: "mailtopassowrd@gmail.com", // generated ethereal user
+    pass: "Ahmedayman12" // generated ethereal password
   }
 });
-
 function createToken(user) {
   return jwt.sign(
-    { id: user.id, email: user.email,avatar:user.avatar, role: user.role },
+    { id: user.id, email: user.email, avatar: user.avatar, role: user.role },
     config.jwtSecret,
     {
-      expiresIn: 200 // 86400 expires in 24 hours
+      expiresIn: 8640 // 86400 expires in 24 hours
     }
   );
 }
@@ -112,114 +115,61 @@ exports.updateUser = async (req, res) => {
   }
 };
 
-exports.resetPassword = (req, res) => {
-    console.log(req.body.email)
-  User.findOne({
-     email: req.body.email  //checking if the email address sent by client is present in the db(valid)
-  }).then(function(user) {
+exports.resetPassword = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
     if (!user) {
-      return res.status(500).json({msg: "No user found with that email address."});
+      return res.status(400).json({ msg: "user does not exits!" });
     }
-    ResetPassword.findOne({
-  user: user._id 
-    }).then(async function(resetPassword) {
-      if (resetPassword)
-      try {
-        await ResetPassword.findByIdAndDelete(resetPassword._id);
-
-      } catch (error) {
-          console.log(error);
-      }
-      let token = crypto.randomBytes(32).toString("hex");
-      bcrypt.hash(token, null, null, function(err, hash) {
-        //hashing the password to store in the db node.js
-        ResetPassword.create({
-          user: user._id,
-          resetPasswordToken: hash,
-          expire: moment.utc().add(config.tokenExpiry, "seconds")
-        }).then(function(item) {
-          if (!item)
-            return throwFailed(
-              res,
-              "Oops problem in creating new password record"
-            );
-          let mailOptions = {
-            from: "e7tiaty2001@gmail.com",
-            to: user.email,
-            subject: "Reset your account password",
-            html:
-              "<h4><b>Reset Password</b></h4>" +
-              "<p>To reset your password, complete this form:</p>" +
-              "<a href=" +
-              config.clientUrl +
-              "reset/" +
-              user.id +
-              "/" +
-              token +
-              '">' +
-              config.clientUrl +
-              "reset/" +
-              user.id +
-              "/" +
-              token +
-              "</a>" +
-              "<br><br>" +
-              "<p>--Team</p>"
-          };
-          let mailSent = sendMail(mailOptions); //sending mail to the user where he can reset password.User id and the token generated are sent as params in a link
-          if (mailSent) {
-            return res.json({
-              success: true,
-              message: "Check your mail to reset your password."
-            });
-          } else {
-            return throwFailed(error, "Unable to send email.");
-          }
-        }).catch(err=>console.log(err));
-      });
-    });
-  });
+    let paylod = {
+      id: user._id,
+      email: user.email
+    };
+    // current password hash from the database, and combine it
+    // with the user's created date to make a very unique secret key!
+    let secret = user.password;
+    let token = jwt.sign(paylod, secret);
+    let mailOptions = {
+      from: "e7tiaty2001@gmail.com",
+      to: user.email,
+      subject: "Reset your account password",
+      html:
+        "<h4><b>Reset Password</b></h4>" +
+        "<p>To reset your password, complete this form:</p>" +
+        "<a href=" +
+        "https://shrieking-ghost-72713.herokuapp.com/" +
+        "reset/" +
+        user.id +
+        "/" +
+        token +
+        '">' +
+        "click here" +
+        "</a>" +
+        "<br><br>" +
+        "<p>--Team</p>"
+    };
+    let mailSent = transporter.sendMail(mailOptions, function(err, info) {
+      if (err) console.log(err);
+      else console.log(info);
+      return res
+        .status(200)
+        .json({ msg: "email sent with info to reset password" });
+    }); //sending mail to the user where he can reset password.User id and the token generated are sent as params in a link
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-exports.SaveNewPassword = (req, ress) => {
+exports.SaveNewPassword = async (req, ress) => {
   const userId = req.body.userId;
   const token = req.body.token;
   const password = req.body.password;
-  ResetPassword.findOne({ where: { user: userId} })
-    .then(function(resetPassword) {
-      if (!resetPassword) {
-        return throwFailed(res, "Invalid or expired reset token.");
-      }
-      bcrypt.compare(token, resetPassword.token, function(
-        errBcrypt,
-        resBcrypt
-      ) {
-        // the token and the hashed token in the db are verified befor updating the password
-        let expireTime = moment.utc(resetPassword.expire);
-        let currentTime = new Date();
-        bcrypt.hash(password, null, null, function(err, hash) {
-          User.update(
-            {
-              password: hash
-            },
-            { where: { id: userId } }
-          ).then(() => {
-            ResetPassword.update(
-              {
-                status: 1
-              },
-              { where: { id: resetPassword.id } }
-            ).then(msg => {
-              if (!msg) throw err;
-              else
-                res.json({
-                  success: true,
-                  message: "Password Updated successfully."
-                });
-            });
-          });
-        });
-      });
-    })
-    .catch(error => throwFailed(error, ""));
+  try {
+    let user = await  User.findById(userId);
+    user.password=password;
+    await User.findByIdAndUpdate();
+    return res.status(200).json({msg:"Password Updated Successfully!"})
+  } catch (error) {
+    console.log(error);
+  }
 };
